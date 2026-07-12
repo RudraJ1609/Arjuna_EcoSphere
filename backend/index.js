57a13d8ff72bdbb39b01de0b439f3022e6629710
RudraJ1609 <jadavrudra838@gmail.com>
2026-07-12 13:00:14 +0530

    Database connected

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./Config/database');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('EcoSphere API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
